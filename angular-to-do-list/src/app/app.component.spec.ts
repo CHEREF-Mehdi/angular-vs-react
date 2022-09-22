e1b108f589c4e66ae7f93ce02a84d34dbfee8979
CHEREF Mehdi <cheref-mehdi@hotmail.fr>
2022-09-23 01:02:02 +0200

    bootstrapped apps

--- angular-to-do-list/src/app/app.component.spec.ts
+++ angular-to-do-list/src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angular-to-do-list'`, () => {
+  it(`should have as title 'angular-app'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-to-do-list');
+    expect(app.title).toEqual('angular-app');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('angular-to-do-list app is running!');
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('.content span')?.textContent).toContain('angular-app app is running!');
   });
 });
